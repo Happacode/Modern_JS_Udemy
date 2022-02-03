df1a4bd71bd1e50912f310e2a9fd5e8ecd37c804
Harp Sandhu <happacode@protonmail.com>
2022-02-03 15:37:38 -0800

    data types: primative and reference objects

---
+++ js_sandbox/app_3.js
@@ -0,0 +1,29 @@
+// Data Types
+
+// Primative:
+
+// String
+const username = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null 
+const car = null;
+// Undefinded
+let test;
+//Symbol
+const sym = Symbol();
+
+// Reference Type - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);
