68195b262b2143db91183c6de52abcfa907dafca
Harp Sandhu <happacode@protonmail.com>
2022-03-12 14:16:16 -0800

    examining the document object

--- js_sandbox_dom/app_1.js
+++ js_sandbox_dom/app_1.js
@@ -1,1 +1,40 @@
 // Examining The Document Object
+let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts); // converts scripts to an Array so we can use .forEach method
+
+scriptsArr.forEach(function(script) {
+  console.log(script);
+});
+
+console.log(val);
